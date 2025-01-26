bd790419969bfaa7c118f086e4d0a981fc05f14a
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-01-26 15:21:35 +0100

    refactor: simplify docstring and update print format in say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,24 +1,6 @@
-#!/usr/bin/python3
 """
-    To define a function that prints "My name is <first name> <last name>"
+This module defines a function that prints a name.
 """
-
-# Project Title
-
-This is a sample project to demonstrate code corrections and formatting.
-
-## Description
-
-This project includes several Python scripts and tests.
-
-### Usage
-
-To use the scripts, run the following commands:
-
-```sh
-python3 0-main.py
-python3 3-main.py
-```
 
 def say_my_name(first_name, last_name=""):
     """
@@ -28,14 +10,12 @@ def say_my_name(first_name, last_name=""):
         first_name (str): The first name.
         last_name (str): The last name.
 
-    Raise:
-        TypeError: if first_name and last_name are not a string.
+    Raises:
+        TypeError: If first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print(f"My name is {first_name} {last_name}")
-
-print(add_integer(2, 3))
+    
+    print("My name is {} {}".format(first_name, last_name))
