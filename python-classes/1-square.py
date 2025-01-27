3758fb0412548b52ad69b6e6e6e40ca5bab3a10b
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-01-27 13:13:13 +0100

    feat: implement Square class with private size attribute and initial test in main file

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with a private instance attribute: size.
+"""
+
+class Square:
+    """
+    A class that defines a square with a private size attribute.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
