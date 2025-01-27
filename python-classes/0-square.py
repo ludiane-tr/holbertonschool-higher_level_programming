4156d83b47c67d5188902d66888bc0a6c7b6992c
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-01-27 11:52:55 +0100

    feat: add module docstring and enhance class docstring in 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class Square.
+"""
+
 
 class Square:
-    """Represents a square."""
+    """
+    This class represents an empty definition of a square.
+    """
     pass
