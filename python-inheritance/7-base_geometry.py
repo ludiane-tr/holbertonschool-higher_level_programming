c8933d544e3bf07301f4c6c25aab900f8db240cd
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-02-05 16:26:19 +0100

    fix: improve integer_validator method to handle boolean and non-integer types

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,15 +3,12 @@
 
 
 class BaseGeometry:
-    """Base class for geometry operations"""
-
+    '''class BaseGemetry'''
     def area(self):
-        """Raise an exception indicating the method is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that 'value' is an integer greater than 0"""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int) or type(value) is bool:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
