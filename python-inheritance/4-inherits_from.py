b4b6f5f643eba052b0c31ef80a2298d11804dbf4
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-02-05 10:44:06 +0100

    feat: add inherits_from function to check class inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """
+This module defines a function inherits_from.
+The function checks if an object is an instance of a class
+that inherited from the specified class.
 """
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that
+    inherited from the specified class.
+    
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        True if obj is an instance of a class that inherits from a_class,
+        False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
