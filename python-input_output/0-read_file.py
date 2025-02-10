b1b1cb25190191910a3c8da0f3c2e550b1362651
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-02-10 10:56:52 +0100

    feat: add docstrings to read_file and write_file functions for better clarity

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,12 @@ Function that reads a text file (UTF8) and prints it to stdout
 
 
 def read_file(filename=""):
- with open(filename, encoding="utf-8") as f:
+    """
+    Reads a text file (UTF-8) and prints its contents to stdout.
+
+    Args:
+        filename (str): The name of the file to be read.
+        Defaults to an empty string.
+    """
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,19 @@ Function that writes a string to a text file and return the len
 
 
 def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8).
+    Returns the number of characters written.
+
+    Args:
+        filename (str): The name of the file to write to.
+        Defaults to an empty string.
+        text (str): The string to write to the file.
+        Defaults to an empty string.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
     with open(filename, "w", encoding="UTF-8") as file:
         file.write(text)
     return len(text)
