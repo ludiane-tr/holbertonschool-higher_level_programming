bd91b7f61f08b78694f34384c5981140389a4f84
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-02-10 10:30:31 +0100

    fix: remove unnecessary blank line in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Function that reads a text file (UTF8) and prints it to stdout'''
 
+
 def read_file(filename=""):
     with open(filename, "r", encoding="UTF-8") as file:
         for words in file:
