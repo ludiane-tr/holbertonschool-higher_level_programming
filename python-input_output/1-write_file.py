63d2475e13b60b727d24160826fe4ba124c01b2f
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-02-10 10:42:29 +0100

    feat: update write_file function to return the length of the written string

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''Function that writes a string to a text file'''
+'''Function that writes a string to a text file and return the len'''
 
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="UTF-8") as file:
         file.write(text)
-        return len(text)
+    return len(text)
